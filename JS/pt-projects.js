33323052d1d2408a01ad8358aa0e0a3daf2447db
Junior <216026633@student.uj.ac.za>
2020-11-14 07:05:04 +0200

    Additions

--- JS/pt-projects.js
+++ JS/pt-projects.js
@@ -2,24 +2,39 @@
 let topNav = document.getElementById('topNav');
 let home = document.getElementById('home');
 let aboutUs = document.getElementById('About-Us');
+let aboutUsSect = document.getElementById('aboutUs-cont');
 let service = document.getElementById('Service');
+let serviceSect = document.getElementById('service-cont');
 let projects = document.getElementById('Projects');
+let projectsSect = document.getElementById('project-cont');
 let contactUs = document.getElementById('Contact-Us');
+let contactUsSect = document.getElementById('contactUs-cont');
 
-window.onscroll = function navClassAdder(){
+window.onscroll = function(){
+	let y = document.documentElement.scrollTop;
 	if(document.documentElement.scrollTop > 20){
 		topNav.classList.add('navBgColor');
 	}else{
 		topNav.classList.remove('navBgColor');
 	}
+	
+	//console.log(`${y}`);
 }
 
 //scrollTop function
 let scrollTop = document.getElementById('scrollTop');
 
-scrollTop.onclick = function(){
+let line = document.getElementsByClassName('line');
+
+line[0].classList.add('fullLine');
+
+function scroller(){
 	let pagePos = 0;
 	let scrollTopLoop = setInterval(scrollTopper, 20);
+	
+	for(i = 0; i < line.length; i++){
+		line[i].classList.remove('fullLine');
+	}
 	
 	function scrollTopper(){
 		if(document.documentElement.scrollTop === 0){
@@ -28,4 +43,77 @@ scrollTop.onclick = function(){
 			scrollBy(0, pagePos--);
 		}
 	}
+}
+
+scrollTop.onclick = function(){
+	scroller();
+}
+
+home.onclick = function(event){
+	event.preventDefault();
+	scroller();
+	this.children[0].children[1].classList.add('fullLine');
+}
+
+function navToSectScroller(section){
+	let sect = section;
+	let pagePos = 0;
+	let scrollerLooper = setInterval(scroller, 20);
+	
+
+	for(i = 0; i < line.length; i++){
+		line[i].classList.remove('fullLine');
+	}
+
+	function scroller(){
+		let y = document.documentElement.scrollTop;
+		let x = sect.offsetTop - topNav.clientHeight;
+		
+		if(y < x){
+			
+			scrollBy(0, pagePos++);
+		}else{
+			console.log(`${y} / ${x}`);
+			clearInterval(scrollerLooper);
+		}
+	}
+	
+	let scrollerUpLooper = setInterval(scrollerUp, 20);
+	
+	function scrollerUp(){
+		let y = document.documentElement.scrollTop;
+		let x = sect.offsetTop - topNav.clientHeight;
+
+		if(y > x){
+			scrollBy(0, pagePos--);
+		}else{
+			console.log(`${y} / ${x}`);
+			clearInterval(scrollerUpLooper);
+		}
+	}
+}
+
+aboutUs.onclick = function(){
+	event.preventDefault();
+	console.log(topNav.clientHeight);
+	navToSectScroller(aboutUsSect);
+	this.children[0].children[1].classList.add('fullLine');
+}
+
+service.onclick = function(){
+	event.preventDefault();
+	navToSectScroller(serviceSect);
+	this.children[0].children[1].classList.add('fullLine');
+}
+
+projects.onclick = function(){
+	event.preventDefault();
+	navToSectScroller(projectsSect);
+	this.children[0].children[1].classList.add('fullLine');
+}
+
+contactUs.onclick = function(){
+	event.preventDefault();
+	navToSectScroller(contactUsSect);
+	this.children[0].children[1].classList.add('fullLine');
 }
