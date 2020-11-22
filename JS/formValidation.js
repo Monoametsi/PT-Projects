299c79f087079d6026efae4adbbe969de37ce558
Junior <216026633@student.uj.ac.za>
2020-11-22 13:08:28 +0200

    Additions

--- JS/formValidation.js
+++ JS/formValidation.js
@@ -15,29 +15,45 @@ function nameValidation(){
 		nameErr.style.display = 'flex';
 		nameErr.innerHTML = 'Required';
 		nameField.classList.add('error');
+		return false;
+
 	}else{
 		nameErr.style.display = 'none';
 		nameField.classList.remove('error');
+		return true;
+
 	}
 }
 
-/*emailField.oninput = function(){
+emailField.oninput = function(){
 	emailValidation();
 }
 
 function emailValidation(){
 	let emailFieldVal = emailField.value;
 	let emailErr = document.getElementById('errorEmail-message');
+	let emailRegEx = /^\w+([.!#$%&'*+-/=?^_`{|}~]?\w+)*@[A-Za-z0-9]+[-]?[A-Za-z0-9]+\.[A-Za-z]{2,3}(?:\.[A-Za-z]{2})*(?:\.[A-Za-z]{2}\.[A-Za-z]{2})*$/;
+	let regExTest = emailRegEx.test(emailFieldVal);
 	
-	if(nameFieldVal === ''){
-		nameErr.style.display = 'flex';
-		nameErr.innerHTML = 'Required';
-		nameField.classList.add('error');
+	if(emailFieldVal === ''){
+		emailErr.style.display = 'flex';
+		emailErr.innerHTML = 'Required';
+		emailField.classList.add('error');
+		return false;
+
+	}else if(regExTest === false){
+		emailErr.style.display = 'flex';
+		emailErr.innerHTML = 'Invalid';
+		emailField.classList.add('error');
+		return false;
+
 	}else{
-		nameErr.style.display = 'none';
-		nameField.classList.remove('error');
+		emailErr.style.display = 'none';
+		emailField.classList.remove('error');
+		return true;
+
 	}
-}*/
+}
 
 msgField.oninput = function(){
 	msgValidation();
@@ -51,8 +67,36 @@ function msgValidation(){
 		msgErr.style.display = 'flex';
 		msgErr.innerHTML = 'Required';
 		msgField.classList.add('error');
+		return false;
+
 	}else{
 		msgErr.style.display = 'none';
 		msgField.classList.remove('error');
+		return true;
+
+	}
+}
+
+subBtn.onclick = function(){
+	event.preventDefault();
+
+	nameValidation();
+	msgValidation();
+	emailValidation();
+
+	if(nameValidation() == false){
+		nameValidation();
+		return false;
+
+	}else if(msgValidation() == false){
+		msgValidation();
+		return false;
+
+	}else if(emailValidation() == false){
+		emailValidation();
+		return false;
+
+	}else{
+		return true;
 	}
 }
