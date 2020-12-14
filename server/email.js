a34af30f98dde14609f64b6db3affdb7edf86543
Junior <216026633@student.uj.ac.za>
2020-12-14 23:05:58 +0200

    adds and deletions

---
+++ server/email.js
@@ -0,0 +1,28 @@
+const nodeMailer = require('nodemailer');
+
+let transporter = nodeMailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: '216026633@student.uj.ac.za',
+		pass: '12345678MMose@1'
+	}
+});
+
+const mailDeliverer = (name,email,message) => {
+	let mailOptions = {
+		from: email,
+		to: '216026633@student.uj.ac.za',
+		subject: 'Form submission',
+		html: `User Name: ${name} <br><br> User Messgae: ${message}`
+	};
+
+	transporter.sendMail(mailOptions, (err, content) => {
+		if(err){
+			console.log(err);
+		}else{
+			console.log('Email sent to :' + content.response);
+		}
+	});
+}
+
+module.exports = mailDeliverer;
